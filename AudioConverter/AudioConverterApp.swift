32223e3cd2cbcd30c34781291929a6a6b40d30b4
Mike Schreiber <schreiber.mike@outlook.com>
2021-02-15 20:09:21 -0800

    Initial Commit

---
+++ AudioConverter/AudioConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioConverterApp.swift
+//  AudioConverter
+//
+//  Created by Mike Schreiber on 2/15/21.
+//
+
+import SwiftUI
+
+@main
+struct AudioConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
